c9d572c30f453c6584eef6fbbc6193494c2a0bee
Nayem-Hossain <nayem.hossain7298@gmail.com>
2023-02-08 17:32:40 +0600

    add vercel.json file

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ async function run() {
       const sslcommer = new SSLCommerzPayment(
         process.env.STORE_ID,
         process.env.STORE_PASSWORD,
-        true
+        false
       ); //true for live default false for sandbox
       sslcommer.init(data).then((data) => {
         //process the response that got from sslcommerz
@@ -106,7 +106,9 @@ async function run() {
     app.post("/success", async (req, res) => {
       console.log(req);
       // res.status(200).json(req.body);
-      res.status(200).redirect(`https://fast-drive-nayem.netlify.app/order_success`);
+      res
+        .status(200)
+        .redirect(`https://fast-drive-nayem.netlify.app/order_success`);
     });
     // GET ALL PRODUCTS API
     app.get("/products", async (req, res) => {
